ee1db05db8b53df316bc1030f0e0d92884e6ba47
Alek Kruk <kruk.aleksander@hotmail.com>
2021-01-03 09:18:07 +0100

    add useReducer values to components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import Navbar from './Navbar';
 import CartContainer from './CartContainer';
 
 const App = () => {
+  const { loading } = useGlobalContext();
+  if (loading) {
+    return <div className="loading">
+      <h1>Loading...</h1>
+    </div>
+  }
   return (
     <main>
       <Navbar />
